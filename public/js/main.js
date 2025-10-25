7ced4bef6406f157167e1c66b42badca3d5427a0
Daniel Oceno <daniel.oceno@outlook.com>
2025-10-24 22:47:40 -0400

    improve css, js and html

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@
   document.addEventListener("copy", (e) => e.preventDefault());
   document.addEventListener("cut", (e) => e.preventDefault());
   document.addEventListener("paste", (e) => e.preventDefault());
+  const donateButtonsContainer = document.getElementById("donate-buttons");
 
   const q = (e) => document.getElementById(e),
     input = q("inputText"),
@@ -46,14 +47,21 @@
   };
 
   // Donate buttons
-  document.querySelectorAll(".donate").forEach((e) => {
-    e.onclick = () =>
+  const donationAmounts = [1, 5, 10];
+  donationAmounts.forEach((amount) => {
+    const button = document.createElement("button");
+    button.className = "donate-buttons button";
+    button.dataset.amount = amount;
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" viewBox="0 0 24 24"><path d="M23.5 6.5c-.3-2.3-2.4-3.5-5.6-3.5h-8.8c-.4 0-.8.3-.9.7l-3.2 15.3c-.1.5.3.9.8.9h3.8l.8-4h2.4c4.6 0 8.1-2.3 9.1-6.4.1-.5.2-1 .2-1.5z" fill="skyblue"/></svg>$${amount}`;
+    button.onclick = () => {
       window.open(
-        `https://www.paypal.com/donate?business=daniel.oceno@gmail.com&amount=${e.dataset.amount}&currency_code=USD`,
+        `https://www.paypal.com/donate?business=daniel.oceno@gmail.com&amount=${amount}&currency_code=USD`,
         "_blank"
       );
+    };
+    donateButtonsContainer.appendChild(button);
   });
-
+  
   // Year
   q("year").textContent = new Date().getFullYear();
 
